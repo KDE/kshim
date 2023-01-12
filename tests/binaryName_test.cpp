164079224f0d7cce555f0f4195928398f1f62d2c
Hannah von Reth <vonreth@kde.org>
2023-01-12 10:33:03 +0100

    Fix test on Windows

--- tests/binaryName_test.cpp
+++ tests/binaryName_test.cpp
@@ -5,7 +5,9 @@ int main()
     KLog::setLoggingEnabled(true);
     KLog::setStdLoggingEnabled(true);
     const auto binaryName = KShimLib::binaryName();
-    TEST_EQ(binaryName, KShimTest::binaryDir() / KSTRING("binaryName_test"s));
+    TEST_EQ(binaryName,
+            (KShimTest::binaryDir() / KSTRING("binaryName_test"s))
+                    .replace_extension(KShimLib::exeSuffix));
     kLog << "End";
     return 0;
 }
